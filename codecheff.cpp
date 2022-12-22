1f585e45880fbb67cfb4405eee54add4f5e64337
Soorajm100 <soorajm825@gmail.com>
2022-12-22 17:52:00 +0530

    codecheff template

---
+++ codecheff.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n";
+
+void solve()
+{
+    
+
+}
+
+int main()
+{
+    ll test;
+    cin>>test;
+
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
