baad80d37c9f21e4985a2b2b4a233248fe00514b
Soorajm100 <soorajm825@gmail.com>
2022-12-28 19:25:12 +0530

    Create googlekickstarttemplate.cpp

---
+++ googlekickstarttemplate.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+
+typedef long long ll;
+using namespace std;
+
+
+
+void solve()
+{
+    
+
+}
+int main()
+{
+	
+	ll t = 1;
+	cin >> t;
+	ll test = 1;
+	while(t--)
+	{
+		cout << "Case #" << test << ": ";
+		solve();
+		test++;
+	}
+} 
+
